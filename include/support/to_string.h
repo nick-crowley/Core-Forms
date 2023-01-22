26d23678074c98a713e941d817712b004a7a9e40
nick-crowley <nicholas.crowley@gmail.com>
2023-01-22 17:40:58 +0000

    refactor: Simplified implementations of `forms::to_string()` using new function objects

--- include/support/to_string.h
+++ include/support/to_string.h
@@ -1,37 +1,30 @@
 #pragma once
 #include "library/core.Forms.h"
+#include "nstd/Functional.h"
 
 namespace core::forms
 {
-	namespace detail {
-		template <typename Output>
-		auto inline to_static_cast = [](auto&& value) { return static_cast<Output>(std::forward<decltype(value)>(value)); };
-
-		inline const auto to_narrow = to_static_cast<char>; // [](auto& wc) { return static_cast<char>(wc); };
-		inline const auto to_wide = to_static_cast<wchar_t>; // [](auto& c) { return static_cast<wchar_t>(c); };
-	}
-
 	std::string 
 	inline to_string(std::wstring const& ws) {
 		return { 
-			boost::make_transform_iterator(ws.begin(), detail::to_narrow),
-			boost::make_transform_iterator(ws.end(), detail::to_narrow)
+			boost::make_transform_iterator(ws.begin(), nstd::convert_to<char>),
+			boost::make_transform_iterator(ws.end(), nstd::convert_to<char>)
 		};
 	}
 
 	std::string 
 	inline to_string(std::wstring_view const& wsv) {
 		return { 
-			boost::make_transform_iterator(wsv.begin(), detail::to_narrow),
-			boost::make_transform_iterator(wsv.end(), detail::to_narrow)
+			boost::make_transform_iterator(wsv.begin(), nstd::convert_to<char>),
+			boost::make_transform_iterator(wsv.end(), nstd::convert_to<char>)
 		};
 	}
 
 	std::string 
 	inline to_string(gsl::cwzstring const ws) {
 		return { 
-			boost::make_transform_iterator(ws, detail::to_narrow),
-			boost::make_transform_iterator(ws + wcslen(ws), detail::to_narrow)
+			boost::make_transform_iterator(ws, nstd::convert_to<char>),
+			boost::make_transform_iterator(ws + wcslen(ws), nstd::convert_to<char>)
 		};
 	}
 
