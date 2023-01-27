7115f6f3f964958b27d751cdad01a69f39b78fe9
nick-crowley <nicholas.crowley@gmail.com>
2023-01-27 20:32:29 +0000

    feat: Extended `forms::DialogItemTemplate` with helper methods for detecting subclassed controls and adding relevant `OwnerDraw` flags

--- include/dialogs/DialogItemTemplate.h
+++ include/dialogs/DialogItemTemplate.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "library/core.Forms.h"
+#include "controls/StaticStyle.h"
+#include "controls/ButtonStyle.h"
+#include "controls/ListBoxStyle.h"
 #include "system/ResourceId.h"
 #include "graphics/Rectangle.h"
 #include "windows/WindowStyle.h"
@@ -17,5 +20,28 @@ namespace core::forms
 		std::optional<ResourceId>    Title;
 		std::optional<ResourceId>    ClassName;
 		std::vector<std::byte>       Data;
+
+	public:
+		bool
+		subclassed() const {
+			return this->ClassName 
+			    && !this->ClassName->is_numeric() 
+			    && this->ClassName->as_string().starts_with(L"CUSTOM.");
+		}
+		
+	public:
+		void
+		setOwnerDraw() {
+			ResourceId const static CustomStatic(L"Custom.STATIC");
+			ResourceId const static CustomButton(L"Custom.BUTTON");
+			ResourceId const static CustomListBox(L"Custom.LISTBOX");
+
+			if (this->ClassName == CustomStatic)
+				this->Style = (this->Style & ~StaticStyle::TypeMask) | StaticStyle::OwnerDraw;
+			else if (this->ClassName == CustomButton)
+				this->Style = (this->Style & ~ButtonStyle::TypeMask) | ButtonStyle::OwnerDraw;
+			else if (this->ClassName == CustomListBox)
+				this->Style |= ListBoxStyle::OwnerDrawFixed | ListBoxStyle::HasStrings;
+		}
 	};
 }
