8cf443d7a8e457d6a6953e444bf60dfd3f5304a4
nick-crowley <nicholas.crowley@gmail.com>
2023-01-27 20:08:18 +0000

    style: Reformatted `forms::DialogTemplate` class

--- include/dialogs/DialogTemplate.h
+++ include/dialogs/DialogTemplate.h
@@ -13,22 +13,21 @@ namespace core::forms
 		nstd::bitset<WindowStyle>    Style;
 		Rect                         Area;
 		uint32_t                     Ident;
-		std::optional<ResourceId> Title;
-		std::optional<ResourceId> ClassName;
+		std::optional<ResourceId>    Title;
+		std::optional<ResourceId>    ClassName;
 		std::vector<std::byte>       Data;
 	};
 
-	// FIXME: DialogTemplate fields need reformatting due to rename operations
 	struct DialogTemplate {
 		std::optional<uint32_t>         HelpId;
 		nstd::bitset<ExWindowStyle>     ExtendedStyle;
 		nstd::bitset<WindowStyle>       Style;
 		uint16_t                        NumControls;
 		Rect                            Area;
-		std::optional<ResourceId>    Menu;
-		std::optional<ResourceId>    ClassName;
-		std::optional<ResourceId>    Title;
-		std::optional<ResourceId>    Font;
+		std::optional<ResourceId>       Menu;
+		std::optional<ResourceId>       ClassName;
+		std::optional<ResourceId>       Title;
+		std::optional<ResourceId>       Font;
 		std::optional<uint16_t>         Height;
 		std::optional<uint16_t>         Weight;
 		std::optional<uint8_t>          Italic;
