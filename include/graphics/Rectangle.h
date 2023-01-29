0cb1305eb85de9550272ae6c13765ac9a364aa8d
nick-crowley <nicholas.crowley@gmail.com>
2023-01-29 17:55:00 +0000

    refactor: Separated `forms::Border` into its own header file `<graphics/border.h>`

--- include/graphics/Rectangle.h
+++ include/graphics/Rectangle.h
@@ -1,75 +1,10 @@
 #pragma once
 #include "library/core.Forms.h"
+#include "Border.h"
 #include "SizePoint.h"
 
 namespace core::forms
 {
-	struct Border 
-	{
-		Border const
-		static None;
-
-		::LONG const Left = 0, Top = 0, Right = 0, Bottom = 0;
-
-		constexpr
-		explicit
-		Border(GuiMeasurement amount) noexcept 
-		  : Left{amount}, Top{amount}, Right{amount}, Bottom{amount}
-		{}
-
-		constexpr
-		explicit
-		Border(GuiMeasurement horz, GuiMeasurement vert) noexcept 
-		  : Left{horz}, Top{vert}, Right{horz}, Bottom{vert}
-		{}
-		
-		constexpr
-		explicit
-		Border(GuiMeasurement left, GuiMeasurement top, GuiMeasurement right, GuiMeasurement bottom) noexcept 
-		  : Left{left}, Top{top}, Right{right}, Bottom{bottom}
-		{}
-
-		constexpr 
-		explicit
-		Border(Size const& amount) noexcept 
-		  : Left{amount.Width}, Top{amount.Height}, Right{amount.Width}, Bottom{amount.Height}
-		{}
-		
-		satisfies(Border,
-			constexpr IsRegular noexcept,
-			constexpr NotSortable noexcept
-		);
-	
-		Border constexpr
-		operator-() const {
-			return Border{-this->Left, -this->Top, -this->Right, -this->Bottom};
-		}
-	
-		//! @brief	Return scaled version of this border
-		Border constexpr
-		operator*(::LONG scale) const noexcept {
-			return Border{this->Left*scale, this->Top*scale, this->Right*scale, this->Bottom*scale};
-		}
-	};
-
-	Border constexpr
-	operator*(::LONG scale, Border const& b) noexcept {
-		return b * scale;
-	}
-	
-	Border const
-	inline Border::None;
-
-	std::string
-	inline to_string(Border const& b)
-	{
-		if (b.Left == b.Top == b.Right == b.Bottom)
-			return std::format("{{Border={}}}", b.Left);
-		else if (b.Left == b.Right && b.Top == b.Bottom)
-			return std::format("{{Horz={} Vert={}}}", b.Left, b.Top);
-		return std::format("{{{},{},{},{}}}", b.Left, b.Top, b.Right, b.Bottom);
-	}
-
 	class Rect {
 	public:
 		Rect const
