aac57a6b1ecd0eb9a6d6266331a94ad1c1754bcb
nick-crowley <nicholas.crowley@gmail.com>
2023-02-04 13:46:42 +0000

    docs: Documented `forms::ClassStyle` enumeration

--- include/windows/ClassStyle.h
+++ include/windows/ClassStyle.h
@@ -4,21 +4,20 @@
 namespace core::forms
 {
 	enum class ClassStyle : uint32_t {
-		Vredraw				= CS_VREDRAW				, // 0x0001
-		Hredraw				= CS_HREDRAW				, // 0x0002
-		Dblclks				= CS_DBLCLKS				, // 0x0008
-		Owndc				= CS_OWNDC					, // 0x0020
-		Classdc				= CS_CLASSDC				, // 0x0040
-		Parentdc			= CS_PARENTDC				, // 0x0080
-		Noclose				= CS_NOCLOSE				, // 0x0200
-		Savebits			= CS_SAVEBITS				, // 0x0800
-		Bytealignclient		= CS_BYTEALIGNCLIENT		, // 0x1000
-		Bytealignwindow		= CS_BYTEALIGNWINDOW		, // 0x2000
-		Globalclass			= CS_GLOBALCLASS			, // 0x4000
-		Ime					= CS_IME					, // 0x00010000
-		Dropshadow			= CS_DROPSHADOW				, // 0x00020000
-	
-		None				= 0,
+		VRedraw         = CS_VREDRAW,           //!< Redraws the entire window if a movement or size adjustment changes the height of the client area
+		HRedraw         = CS_HREDRAW,           //!< Redraws the entire window if a movement or size adjustment changes the width of the client area
+		DblClks         = CS_DBLCLKS,           //!< Sends a double-click message to the window procedure when the user double-clicks the mouse 
+		OwnDC           = CS_OWNDC,             //!< Allocates a unique device context for each window in the class.
+		ClassDC         = CS_CLASSDC,           //!< Allocates one device context to be shared by all windows in the class, multiplexed between threads
+		ParentDC        = CS_PARENTDC,          //!< Sets the clipping rectangle of the child window to that of the parent window so that the child can draw on the parent
+		NoClose         = CS_NOCLOSE,           //!< Disables @c Close on the window menu
+		SaveBits        = CS_SAVEBITS,          //!< Redraws area beneath the window upon destruction using a stored bitmap instead of sending windows @c WM_PAINT
+		ByteAlignClient = CS_BYTEALIGNCLIENT,   //!< Aligns the window's client area on a byte boundary (in the x direction)
+		ByteAlignWindow = CS_BYTEALIGNWINDOW,   //!< Aligns the window on a byte boundary (in the x direction)
+		GlobalClass     = CS_GLOBALCLASS,       //!< Indicates that the window class is an application global class.
+		Ime             = CS_IME,               //!< Used with Input Method Manager for East Asian localization
+		DropShadow      = CS_DROPSHADOW,        //!< Enables the drop shadow effect on a window if @c SPI_SETDROPSHADOW is enabled
+		None            = 0,
 	};
 }	// namespace core::forms
 
