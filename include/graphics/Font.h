f97a14c7cc0279a37f439c78b6cad84a1d9b3a39
nick-crowley <nicholas.crowley@gmail.com>
2023-02-10 21:36:20 +0000

    feat: Support creating similar fonts

--- include/graphics/Font.h
+++ include/graphics/Font.h
@@ -31,7 +31,34 @@ namespace core::forms
 			else
 				this->Handle.reset(font);
 		}
-	
+		
+		Font(::HFONT                           existing, 
+			 std::optional<std::wstring_view>  name = std::nullopt,
+			 std::optional<int32_t>            twipsHeight = std::nullopt, 
+			 std::optional<FontWeight>         weight = std::nullopt, 
+			 std::optional<bool>               italic = std::nullopt, 
+			 std::optional<bool>               underline = std::nullopt
+		) {
+			::LOGFONT props {};
+			if (!::GetObjectW(existing, sizeof(::LOGFONT), &props))
+				win::LastError{}.throwAlways();
+
+			if (name)
+				wcscpy_s(props.lfFaceName, name->data());
+			if (twipsHeight)
+				props.lfHeight = *twipsHeight;
+			if (weight)
+				props.lfWeight = static_cast<::LONG>(*weight);
+			if (italic)
+				props.lfItalic = static_cast<::BYTE>(*italic);
+			if (underline)
+				props.lfUnderline = static_cast<::BYTE>(*underline);
+			if (auto const font = ::CreateFontIndirectW(&props); !font)
+				win::LastError{}.throwAlways();
+			else
+				this->Handle.reset(font);
+		}
+		
 		::HFONT
 		handle() const
 		{
