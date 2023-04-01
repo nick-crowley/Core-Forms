b8d64d55deb348efc0752ca376dbaefd46557716
nick-crowley <nicholas.crowley@gmail.com>
2023-04-01 15:29:23 +0100

    fix: `forms::DialogTemplateWriter` should calculate the required buffer capacity to the hold the generated template rather than estimating

--- include/dialogs/DialogTemplateWriter.h
+++ include/dialogs/DialogTemplateWriter.h
@@ -8,10 +8,65 @@ namespace core::forms
 {
 	class DialogTemplateWriter : public DialogTemplateTypes
 	{
-		std::byte*	Position = nullptr;
+		std::span<std::byte>	bufferRemaining;
 
 	public:
 		DialogTemplateWriter() = default;
+
+	private:
+		size_t
+		static measureDialogTemplate(DialogTemplate const& dlg) 
+		{
+			// DLG-TEMPLATE
+			auto totalSize = nstd::sizeof_v<TemplateIdent,uint32_t,ExWindowStyle,WindowStyle,uint16_t,SmallRect>
+			               + DialogTemplateWriter::measureResourceIdent(dlg.Menu)
+			               + DialogTemplateWriter::measureResourceIdent(dlg.ClassName)
+			               + DialogTemplateWriter::measureResourceIdent(dlg.Title);
+			
+			// [style & DS_FONT] short ptSize, wchar font-name[]
+			if (dlg.Style.test(DialogStyle::SetFont|DialogStyle::ShellFont)) 
+				totalSize += nstd::sizeof_v<uint16_t,uint16_t,uint8_t,uint8_t> 
+				           + DialogTemplateWriter::measureResourceIdent(dlg.Font);
+
+			// DLG-ITEM-TEMPLATE(s)
+			for (const DialogItemTemplate& ctrl : dlg.Controls) {
+				totalSize += totalSize % sizeof(uint32_t);
+				totalSize += measureItemTemplate(ctrl);
+			}
+
+			return totalSize;
+		}
+		
+		size_t
+		static measureItemTemplate(DialogItemTemplate const& ctrl) 
+		{
+			return nstd::sizeof_v<uint32_t,ExWindowStyle,WindowStyle,SmallRect,uint32_t> 
+			     + DialogTemplateWriter::measureResourceIdent(ctrl.ClassName)
+			     + DialogTemplateWriter::measureResourceIdent(ctrl.Title)
+			     + DialogTemplateWriter::measureBinaryData(ctrl.Data.data(), ctrl.Data.data() + ctrl.Data.size());
+		}
+
+		size_t
+		static measureResourceIdent(std::optional<ResourceId> const& r) 
+		{	
+			if (!r) {
+				return sizeof(MissingIdent);
+			}
+			else if (r->is_numeric()) {
+				return sizeof(NumericIdent);
+			}
+			else 
+				return nstd::sizeof_n<wchar_t>(1+r->as_string().length());
+		}
+		
+		size_t
+		static measureBinaryData(std::byte const* start, std::byte const* finish) 
+		{	
+			if (start == finish)
+				return sizeof(MissingIdent);
+			else 
+				return nstd::sizeof_v<uint16_t,uint16_t> + nstd::sizeof_n<uint8_t>(finish-start);
+		}
 
 	public:
 		DialogTemplateBlob
@@ -20,8 +75,8 @@ namespace core::forms
 			TemplateIdent constexpr 
 			static DLG_TEMPLATE_EX{ 1 };
 
-			std::vector<std::byte> blob(2048);	// TODO: Measure size required by template
-			this->Position = blob.data();
+			std::vector<std::byte> buffer(DialogTemplateWriter::measureDialogTemplate(dlg));
+			this->bufferRemaining = buffer;
 
 			// TODO: Query whether layout requires DLG-TEMPLATE-EX
 			bool const extended = true;
@@ -51,7 +106,7 @@ namespace core::forms
 			this->writeResourceIdent(dlg.Menu);
 			this->writeResourceIdent(dlg.ClassName);
 			this->writeResourceIdent(dlg.Title);
-
+			
 			// [style & DS_FONT] short ptSize, wchar font-name[]
 			if (dlg.Style.test(DialogStyle::SetFont|DialogStyle::ShellFont)) {
 				this->writeObject<uint16_t>(*dlg.Height);
@@ -64,63 +119,77 @@ namespace core::forms
 			}
 
 			// DLG-ITEM-TEMPLATE(s)
-			for (const DialogItemTemplate& ctrl : dlg.Controls)
+			for (DialogItemTemplate const& ctrl : dlg.Controls)
 			{
 				// Ensure each item is aligned on 32-bit boundaries
-				switch (reinterpret_cast<uintptr_t>(this->Position) % sizeof(uint32_t)) {
+				switch (reinterpret_cast<uintptr_t>(this->bufferRemaining.data()) % sizeof(uint32_t)) {
 				case 1: this->writeObject<uint8_t[3]>({0,0,0}); break;
 				case 2: this->writeObject<uint8_t[2]>({0,0}); break;
 				case 3: this->writeObject<uint8_t[1]>({0}); break;
 				}
-
-				if (extended) {
-					this->writeObject<uint32_t>(*ctrl.HelpId);
-					this->writeObject<ExWindowStyle>(ctrl.ExtendedStyle);
-					this->writeObject<WindowStyle>(ctrl.Style);
-				}
-				else {
-					this->writeObject<WindowStyle>(ctrl.Style);
-					this->writeObject<ExWindowStyle>(ctrl.ExtendedStyle);
-				}
-
-				this->writeObject(SmallRect{ 
-					static_cast<int16_t>(ctrl.Area.Left), 
-					static_cast<int16_t>(ctrl.Area.Top), 
-					static_cast<int16_t>(ctrl.Area.width()), 
-					static_cast<int16_t>(ctrl.Area.height()) 
-				});
-
-				if (extended) {
-					this->writeObject<uint32_t>(ctrl.Ident);
-				}
-				else {
-					this->writeObject<uint16_t>(ctrl.Ident);
-				}
-
-				// Class & Title & Data fields must be aligned on 16-bit boundary
-				//   https://learn.microsoft.com/en-us/windows/win32/api/Winuser/ns-winuser-dlgitemtemplate
-				this->writeResourceIdent(ctrl.ClassName);
-				this->writeResourceIdent(ctrl.Title);
-				this->writeBinaryData(ctrl.Data.data(), ctrl.Data.data() + ctrl.Data.size());
+				
+				this->writeItemTemplate(ctrl);
 			}
 
-			// TODO: Measure size required by dialog template
-			assert(this->Position-blob.data() < (ptrdiff_t)blob.size());
-
-			return {blob.data(), this->Position};
+			return {buffer.data(), this->bufferRemaining.data()};
 		}
 
 	private:
+		void
+		writeItemTemplate(DialogItemTemplate const& ctrl) {
+			Invariant(this->bufferRemaining.size() >= measureItemTemplate(ctrl));
+			
+			// TODO: Query whether layout requires DLG-TEMPLATE-EX
+			bool const extended = true;
+			
+			if (extended) {
+				this->writeObject<uint32_t>(*ctrl.HelpId);
+				this->writeObject<ExWindowStyle>(ctrl.ExtendedStyle);
+				this->writeObject<WindowStyle>(ctrl.Style);
+			}
+			else {
+				this->writeObject<WindowStyle>(ctrl.Style);
+				this->writeObject<ExWindowStyle>(ctrl.ExtendedStyle);
+			}
+
+			this->writeObject(SmallRect{ 
+				static_cast<int16_t>(ctrl.Area.Left), 
+				static_cast<int16_t>(ctrl.Area.Top), 
+				static_cast<int16_t>(ctrl.Area.width()), 
+				static_cast<int16_t>(ctrl.Area.height()) 
+			});
+
+			if (extended) {
+				this->writeObject<uint32_t>(ctrl.Ident);
+			}
+			else {
+				this->writeObject<uint16_t>(ctrl.Ident);
+			}
+
+			// Class & Title & Data fields must be aligned on 16-bit boundary
+			//   https://learn.microsoft.com/en-us/windows/win32/api/Winuser/ns-winuser-dlgitemtemplate
+			this->writeResourceIdent(ctrl.ClassName);
+			this->writeResourceIdent(ctrl.Title);
+			this->writeBinaryData(ctrl.Data.data(), ctrl.Data.data() + ctrl.Data.size());
+		}
+
 		template <typename Object>
 		void
 		writeObject(Object const& value) {
-			auto* object = reinterpret_cast<std::byte const*>(&value);
-			this->Position = std::copy(object, object + sizeof(Object), this->Position);
+			Invariant(this->bufferRemaining.size() >= sizeof(Object));
+
+			auto const* const object = reinterpret_cast<std::byte const*>(&value);
+			this->bufferRemaining = {
+				std::copy(object, object + sizeof(Object), this->bufferRemaining.begin()),
+				this->bufferRemaining.end()
+			};
 		}
 	
 		void
 		writeResourceIdent(std::optional<ResourceId> const& r) 
 		{	
+			Invariant(this->bufferRemaining.size() >= measureResourceIdent(r));
+
 			if (!r) {
 				this->writeObject<MissingIdent>({});
 			}
@@ -134,17 +203,22 @@ namespace core::forms
 				this->writeObject<wchar_t>('\0');
 			}
 		}
-	
+		
 		void
 		writeBinaryData(std::byte const* start, std::byte const* finish) 
 		{	
+			Invariant(this->bufferRemaining.size() >= measureBinaryData(start,finish));
+
 			if (start == finish)
 				this->writeObject<MissingIdent>({});
 			else {
 				// Duplicate the creation-data array size
 				this->writeObject<uint16_t>(static_cast<uint16_t>(finish - start + sizeof(uint16_t)));
 				this->writeObject<uint16_t>(static_cast<uint16_t>(finish - start + sizeof(uint16_t)));		// ABI compatibility with CREATESTRUCT
-				this->Position = std::copy(start, finish, this->Position);
+				this->bufferRemaining = {
+					std::copy(start, finish, this->bufferRemaining.begin()),
+					this->bufferRemaining.end()
+				};
 			}
 		}
 	};
